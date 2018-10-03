c9cc82dcafea8a50a85964eccb326bea5ec2dbd4
Palmera <alegru@gmail.com>
2018-10-03 16:32:04 -0300

    v3

--- Assets/BasicMovement.cs
+++ Assets/BasicMovement.cs
@@ -14,6 +14,7 @@ public class BasicMovement : MonoBehaviour {
     public Rigidbody2D car;
     public int startLane = 0;
     private int currentLane = 0;
+    public bool isBot = true;
     
 	// Use this for initialization
 	void Start () {
@@ -51,19 +52,19 @@ public class BasicMovement : MonoBehaviour {
                 // bezier path
                 Transform start = vec.GetChild(0);
                 Point startPoint = new Point(start.transform.position.x, start.transform.position.y);
-                Debug.Log("startPoint - x " + startPoint.X + " y: " + startPoint.Y);
+                //Debug.Log("startPoint - x " + startPoint.X + " y: " + startPoint.Y);
 
                 Transform ch1 = vec.GetChild(1);
                 Point ch1Point = new Point(ch1.transform.position.x, ch1.transform.position.y);
-                Debug.Log("ch1Point  - x " + ch1Point.X + " y: " + ch1Point.Y);
+                //Debug.Log("ch1Point  - x " + ch1Point.X + " y: " + ch1Point.Y);
 
                 Transform ch2 = vec.GetChild(2);
                 Point ch2Point = new Point(ch2.transform.position.x, ch2.transform.position.y);
-                Debug.Log("ch2Point - x " + ch2Point.X + " y: " + ch2Point.Y);
+                //Debug.Log("ch2Point - x " + ch2Point.X + " y: " + ch2Point.Y);
 
                 Transform end = vec.GetChild(3);
                 Point endPoint = new Point(end.transform.position.x, end.transform.position.y);
-                Debug.Log("endPoint - x " + endPoint.X + " y: " + endPoint.Y);
+                //Debug.Log("endPoint - x " + endPoint.X + " y: " + endPoint.Y);
 
                 nextPath = new BezierPath(startPoint, ch1Point, ch2Point, endPoint);
             } else {
@@ -85,17 +86,61 @@ public class BasicMovement : MonoBehaviour {
         }
         return cPath;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+
+    private bool moveIn() {
+        //Debug.Log("move in: ");
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            // Get movement of the finger since last frame
-            Vector2 touchDeltaPosition = Input.GetTouch(0).position;
-            Debug.Log("touch position: " + touchDeltaPosition);
-            currentLane = currentLane == 0 ? 1 : 0;
+            //Debug.Log("move in touch");
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = touch.position;
+            return touchPosition.x < Screen.width / 2;
+        }
+        bool a = Input.GetKeyDown(KeyCode.LeftArrow);
+        if (a)
+        {
+            Debug.Log("moving in " + a);
+        }
+
+        return Input.GetKeyDown(KeyCode.LeftArrow);
+    }
+    private bool moveOut() {
+        //Debug.Log("move out: --------------------------------");
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            //Debug.Log("move out touch");
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = touch.position;
+            return touchPosition.x > Screen.width / 2;
+        }
+        bool a = Input.GetKeyDown(KeyCode.RightArrow);
+        if (a)
+        {
+            Debug.Log("moving out " + a);
+        }
+        return Input.GetKeyDown(KeyCode.RightArrow);
+    }
+    
+	void Update () {
+        if (!isBot)
+        {
+            if (moveIn())
+            {
+                currentLane = currentLane == 0 ? 0 : currentLane--;
+                Debug.Log("lane " + currentLane);
+            }
+            else if (moveOut())
+            {
+                int maxLanes = track.childCount -1;
+                currentLane = currentLane == maxLanes ? maxLanes : currentLane++;
+                Debug.Log("lane " + currentLane);
+            }
+
             this.path = lanes[currentLane];
         }
+
 
 
         Point pos;
